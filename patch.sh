5a28b3ced50df35fe2a588bf82821445dffcc5b8
Robin <zhaoruibin@aliyun.com>
2020-08-26 12:19:05 +0800

    Update patch.sh

--- patch.sh
+++ patch.sh
@@ -10,10 +10,14 @@ sed -i '/tew-692gr[ \t]*)/i\\txiaoyu,xy-c5|\\' target/linux/ramips/base-files/et
 curl -L https://raw.githubusercontent.com/openwrt/openwrt/master/target/linux/ramips/dts/mt7621_xiaoyu_xy-c5.dts > target/linux/ramips/dts/xiaoyu_xy-c5.dts
 
 echo 'define Device/xiaoyu_xy-c5
+  MTK_SOC := mt7621
   DTS := xiaoyu_xy-c5
   IMAGE_SIZE := $(ralink_default_fw_size_32M)
+  DEVICE_VENDOR := XiaoYu
+  DEVICE_MODEL := XY-C5
   DEVICE_TITLE := XiaoYu XY-C5
-  DEVICE_PACKAGES := kmod-ata-core kmod-ata-ahci kmod-usb3
+  DEVICE_PACKAGES := kmod-ata-core kmod-ata-ahci kmod-usb3 kmod-usb-ledtrig-usbport
+  SUPPORTED_DEVICES += xy-c5
 endef
 TARGET_DEVICES += xiaoyu_xy-c5' >> target/linux/ramips/image/mt7621.mk
 
