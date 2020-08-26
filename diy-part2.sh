1e725675048262bb0f617dee812e616d56dd8717
Robin <zhaoruibin@aliyun.com>
2020-08-27 03:12:38 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -25,7 +25,7 @@ sed -i '/tew-692gr[ \t]*)/i\\txiaoyu,xy-c5|\\' target/linux/ramips/base-files/et
 #sed -i '108acp -f $(TOPDIR)/vermagic $(LINUX_DIR)/.vermagic' include/kernel-defaults.mk
 
 curl -L https://raw.githubusercontent.com/openwrt/openwrt/93b6cc97ca9694434faef69c6c5145b5dc7c1ae4/target/linux/ramips/dts/mt7621_xiaoyu_xy-c5.dts > target/linux/ramips/dts/xiaoyu_xy-c5.dts
-cp kernel-defaults.mk include/kernel-defaults.mk
+cp ../kernel-defaults.mk include/kernel-defaults.mk
 echo 'define Device/xiaoyu_xy-c5
   MTK_SOC := mt7621
   DTS := xiaoyu_xy-c5
