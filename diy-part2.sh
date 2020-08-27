3b7431d1ffc28d38da19b49bc4c4686698909059
Robin <zhaoruibin@aliyun.com>
2020-08-27 12:25:42 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -27,14 +27,15 @@ sed -i '/tew-692gr[ \t]*)/i\\txiaoyu,xy-c5|\\' target/linux/ramips/base-files/et
 curl -L https://raw.githubusercontent.com/robinZhao/openwrt_xy-c5/main/mt7621_xiaoyu_xy-c5.dts > target/linux/ramips/dts/xiaoyu_xy-c5.dts
 curl -L https://raw.githubusercontent.com/robinZhao/openwrt_xy-c5/main/kernel_defaults.mk > include/kernel-defaults.mk
 echo 'define Device/xiaoyu_xy-c5
+define Device/xiaoyu_xy-c5
   MTK_SOC := mt7621
-  DTS := xiaoyu_xy-c5
   IMAGE_SIZE := 32448k
   DEVICE_VENDOR := XiaoYu
-  DEVICE_MODEL := XiaoYu XY-C5
-  DEVICE_TITLE := XiaoYu XY-C5
-  DEVICE_PACKAGES := kmod-ata-core kmod-ata-ahci kmod-usb3 kmod-usb-ledtrig-usbport
+  DEVICE_MODEL := XY-C5
+  DEVICE_PACKAGES := \
+	kmod-ata-core kmod-ata-ahci \
+	kmod-usb3 kmod-usb-ledtrig-usbport
   SUPPORTED_DEVICES += xy-c5
 endef
-TARGET_DEVICES += xiaoyu_xy-c5' >> target/linux/ramips/image/mt7621.mk
+TARGET_DEVICES += xiaoyu_xy-c5 >> target/linux/ramips/image/mt7621.mk
 
